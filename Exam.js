a74c9b0c48343f0982986367b233de76d4bac87e
Jessie Jay Javier <79816400+JavierBSCS@users.noreply.github.com>
2024-09-25 12:29:41 +0800

    Update Exam.js

--- Exam.js
+++ Exam.js
@@ -1,34 +1,65 @@
 //task1
-let name = "Jessie Jay Javier";
-let age = 23;
-let city = "Baguio City";
+
+let name = "//////////";
+let age = "/////////";
+let cityresidence = "/////////";
+
 
 let currentYear = new Date().getFullYear();
-let yearBorn = currentYear - age;
+let yearborn = currentYear - age;
 
-let isAdult = age >= 18;
+let Adult = age >= 18;
 
 let remainder = age % 5;
 
-console.log(name, yearBorn, isAdult, remainder);
+console.log(name, yearborn, Adult, remainder);
 
 
 
 //task 2
-let sentence = name + " from " + city;
+
+let sentence = name + " from " + cityresidence;
+
 
 function toUpperCase(str) {
     return str.toUpperCase();
 }
 
+
 function findFirstA(str) {
     return str.indexOf('a');
 }
+
 
 let originalString = "I am learning JavaScript at School";
 let updatedString = originalString.replace(/JavaScript/g, "JS");
 
 console.log(sentence, toUpperCase(sentence), findFirstA(sentence), updatedString);
+
+
+
+//task 3
+
+
+let favoritefood = ['//////', '////////', '////////', '/////////', '///////'];
+
+
+favoritefood.push('///////');
+
+
+favoritefood.splice(1, 1); 
+
+
+favoritefood.sort();
+
+function printfavoritefood(foods) {
+    for (let food of foods) {
+        console.log(food);
+    }
+}
+
+printfavoritefood(favoritefood);
+
 
 
 
